68598193361b5ce8396d2e32e3d2d909922a079b
KEVIN TAO <26ktao@gmail.com>
2024-06-06 13:38:11 -0700

    day 4

--- day 4/day4.py
+++ day 4/day4.py
@@ -159,3 +159,4 @@ def timeseries():
     rng = pd.date_range("1/1/2012", periods=100, freq="s")
     ts = pd.Series(np.random.randint(0, 500, len(rng)), index=rng)
     print(ts.resample("5Min").sum())
+    
