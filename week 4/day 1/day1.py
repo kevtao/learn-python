b8138ccd5f860566e9f3ac20fb38f1fc0dcb9fe9
KEVIN TAO <26ktao@gmail.com>
2024-07-09 13:03:04 -0700

    breadth width search

--- week 4/day 1/day1.py
+++ week 4/day 1/day1.py
@@ -1,4 +1,4 @@
-array = [5,3,9,2,7,1]
+array = [5, 3, 9, 2, 7, 1]
 
 
 # test usin o(n)
@@ -11,6 +11,8 @@ def largest():
 # largest()
 
 # test using o(n)
+
+
 def findSmallest(arr):
     small = arr[0]
     smallest_index = 0
@@ -21,6 +23,8 @@ def findSmallest(arr):
     return smallest_index
 
 # creating o(n^2)
+
+
 def selectionSort(arr):
     newArr = []
     for i in range(len(arr)):
@@ -28,4 +32,41 @@ def selectionSort(arr):
         newArr.append(arr.pop(small))
     return newArr
 
-print(selectionSort([5, 3, 6, 2, 10]))
+
+# print(selectionSort([5, 3, 6, 2, 10]))
+
+
+# breadth width search
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+def person_is_seller(name):
+ return name[-1] == 'e'
+from collections import deque
+search_queue = deque()
+search_queue += graph["you"]
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print (f"{person} is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+
+search("you")
