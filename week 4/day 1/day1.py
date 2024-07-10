1dee43f586fdd86e7592cd034b5a818f173df6c4
KEVIN TAO <26ktao@gmail.com>
2024-07-10 11:05:27 -0700

    algorithms

--- week 4/day 1/day1.py
+++ week 4/day 1/day1.py
@@ -1,3 +1,4 @@
+from collections import deque
 array = [5, 3, 9, 2, 7, 1]
 
 
@@ -48,11 +49,15 @@ graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
 
+
 def person_is_seller(name):
- return name[-1] == 'e'
-from collections import deque
+    return name[-1] == 'e'
+
+
 search_queue = deque()
 search_queue += graph["you"]
+
+
 def search(name):
     search_queue = deque()
     search_queue += graph[name]
@@ -61,12 +66,60 @@ def search(name):
         person = search_queue.popleft()
         if not person in searched:
             if person_is_seller(person):
-                print (f"{person} is a mango seller!")
+                print(f"{person} is a mango seller!")
                 return True
             else:
                 search_queue += graph[person]
                 searched.append(person)
     return False
 
+# search("you")
 
-search("you")
+
+graph = {}
+graph["you"] = {"alice", "bob", "claire"}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"] = {}
+graph["a"]["fin"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {}
+
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+    print(f"go to {node}")
